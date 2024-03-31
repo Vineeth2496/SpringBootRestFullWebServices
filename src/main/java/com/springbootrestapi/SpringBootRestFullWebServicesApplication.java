ef3577a48a02484aaff595ecc261836984ae46a1
Vineeth2496 <vineetharmoori2496@gmail.com>
2024-03-31 23:47:59 +0530

    Initial commit

---
+++ src/main/java/com/springbootrestapi/SpringBootRestFullWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.springbootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestFullWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestFullWebServicesApplication.class, args);
+	}
+
+}
